8e3249c3e5e9b3d9e618de8d0f7f6ac56d916fca
tkenned2020 <tkenned2020@outlook.com>
2021-06-19 19:31:58 -0400

    added files to the api route

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -14,7 +14,6 @@ const app = express();
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded());
 
 const isProduction = environment === "production";
 
--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,7 +1,37 @@
 const router = require("express").Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./user.js');
 
-router.post("./test", function (req, res) {
-  res.json({ requestBody: req.body });
-});
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+const asyncHandler = require("express-async-handler");
+const { setTokenCookie } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
+
+
+router.get(
+  "/set-token-cookie",
+  asyncHandler(async (req, res) => {
+    const user = await User.findOne({
+      where: {
+        username: "Demo-lition",
+      },
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+  })
+);
+
+const { restoreUser } = require("../../utils/auth.js");
+router.get("/restore-user", restoreUser, (req, res) => res.json(req.user));
+
+const { requireAuth } = require("../../utils/auth.js");
+router.get("/require-auth", requireAuth, (req, res) => res.json(req.user));
+
+// router.post("./test", function (req, res) {
+//   res.json({ requestBody: req.body });
+// });
 
 module.exports = router;
