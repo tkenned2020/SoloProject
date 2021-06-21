262eb97c6c6fbd2ed636683e652970f01c54c9c4
tkenned2020 <tkenned2020@outlook.com>
2021-06-20 22:43:47 -0400

    login form set up in progress

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,13 +1,39 @@
-//all routes live in this folder
 const express = require("express");
 const apiRouter = require("./api");
 const router = express.Router();
 
 router.use("/api", apiRouter);
 
-router.get("/", function(req, res){
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    res.send("Hello World!");
-});
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
 
+  router.get("/", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
+  });
+
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
+  });
+}
+
+if (process.env.NODE_ENV !== "production") {
+  router.get("/api/csrf/restore", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    return res.json({});
+  });
+}
+
+//recently moved to bottom of file 
+router.get("/", function (req, res) {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  res.send("Hello World!");
+});
 module.exports = router;
